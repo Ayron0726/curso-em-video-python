7a3abdd5b7049f13e21b8f3c5bfd4547c00d2961
Ayron0726 <ayron0726@hotmail.com>
2022-10-22 09:16:31 -0300

    Alteração - Inclusão da biblioteca pygame

--- mundo 1/testes/prova.py
+++ mundo 1/testes/prova.py
@@ -1,4 +1,4 @@
-s = 'prova de python'
+"""s = 'prova de python'
 print(len(s))
 
 n1 = 19 // 2
@@ -8,4 +8,10 @@ print(n1)
 print(n2)
 
 resultado = 3*5+4**2
-print(resultado)
+print(resultado)"""
+
+nota1 = input("primeira nota: ")
+nota2 = input("segunda nota: ")
+media = (nota1 + nota2)/2
+
+print("A média é: ",media)
